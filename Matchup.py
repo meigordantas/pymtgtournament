2a9d56da0b331e2ce1298b3e5dd6cfad002fb4e1
Igor Dantas <me.igordantas@gmail.com>
2019-09-03 09:40:19 -0300

    Matchup class

--- Matchup.py
+++ Matchup.py
@@ -1,27 +1,32 @@
 from Player import *
 
+
 class Matchup():
 
     def __init__(self, player_a, player_b, round):
         self.__matchRound = round
+        self.__isFinnished = False
         self.__player_a = player_a
         self.__player_b = player_b
         self.__matchWinner = None
 
     def getWinner(self):
-        return self.__matchWinner 
-    
-    def setMatchRsult(self, result, player):
-        if result in (0,1):
-            return None
-        else:
-            if player in ('a','A','1'):
+        return self.__matchWinner
+
+    def setWinner(self, resultParam):
+        if resultParam in [0, 1, 2]:
+            if resultParam == 0:
+                self.__isFinnished = True
+            elif resultParam == 1:
+                self.__isFinnished = True
                 self.__matchWinner = self.__player_a
-            elif player in ('b','B','2'):
+            else:
                 self.__matchWinner = self.__player_b
+        else:
+            raise ValueError('resultParam must be 0, 1 or 2')
 
-    def getPlayers(self, player):
-        print("%s vs %s" %(self.__player_a.getPlayerName(), self.__player_b.getPlayerName()))
-    
+    def getPlayers(self):
+        print("%s vs %s" % (self.__player_a.getPlayerName(), self.__player_b.getPlayerName()))
 
-    
+
+
--- testApp.py
+++ testApp.py
@@ -1,18 +1,34 @@
-
 from Player import *
 from Tournament import *
+from Matchup import *
+#
+# t = Tournament('Torneio teste 1')
+# print(t.showTournamentUUID())
+#
+# t.addPlayer(Player(12345,'igor'))
+# t.addPlayer(Player(54321,'falcao'))
+# t.addPlayer(Player(15432,'Silvio'))
+#
+# print(t._Tournament__player_arr)
+#
+# print('Showing players')
+# t.showPlayers()
+# t.updateCurrScoreBoard()
+# print('Showing current scoreboard')
+# t.showCurrScoreBoard()
 
-t = Tournament('Torneio teste 1')
-print(t.showTournamentUUID())
+playera = Player(12345,'Player_A')
+playerb = Player(54321,'Player_B')
+mt = Matchup(playera, playerb, 1)
+mt.getPlayers()
 
-t.addPlayer(Player(12345,'igor'))
-t.addPlayer(Player(54321,'falcao'))
-t.addPlayer(Player(15432,'Silvio'))
+try:
+    mt.setWinner(0)
+except ValueError as err:
+    print(err)
 
-print(t._Tournament__player_arr)
+if mt.getWinner() == None:
+    print("Draw")
+else:
+    print("Winner is: %s" % (mt.getWinner().getPlayerName()))
 
-print('Showing players')
-t.showPlayers()
-t.updateCurrScoreBoard()
-print('Showing current scoreboard')
-t.showCurrScoreBoard()
